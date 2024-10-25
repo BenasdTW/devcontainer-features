7e01c6981ac1b03069672142743a73779e37b52f
BenasdTW <ben6312tt@gmail.com>
2024-10-26 03:53:37 +0800

    fix test.sh format 4

--- test/ben-nvim/test.sh
+++ test/ben-nvim/test.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 set -e
 source dev-container-features-test-lib
 # check "validate favorite color" color | grep 'my favorite color is red'
-# check "execute command" bash -c "echo 'hi' | grep 'hi'"
+check "execute command" bash -c "echo 'hi' | grep 'hi'"
 
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.
